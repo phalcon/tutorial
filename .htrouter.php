a10a3781f01616b1ed05955ccc5b8d247e95bf74
Nikolaos Dimopoulos <nikos@niden.net>
2024-01-14 10:20:35 -0600

    htrouter for local

---
+++ .htrouter.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+    return false;
+}
+
+$_GET['_url'] = $_SERVER['REQUEST_URI'];
+
+require_once __DIR__ . '/public/index.php';
