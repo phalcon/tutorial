2e872aa730b20826f258f86c124b7e5bac1a56eb
Nikolaos Dimopoulos <nikos@niden.net>
2026-07-24 09:33:24 -0500

    entry point - better css

--- public/index.php
+++ public/index.php
@@ -1,61 +1,20 @@
 <?php
 
-use Phalcon\Autoload\Loader;
-use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
-use Phalcon\Di\FactoryDefault;
+declare(strict_types=1);
+
 use Phalcon\Mvc\Application;
-use Phalcon\Mvc\Url as UrlProvider;
-use Phalcon\Mvc\View;
 
-define('BASE_PATH', dirname(__DIR__));
-define('APP_PATH', BASE_PATH . '/src');
+$basePath = dirname(__DIR__);
 
-// Register an autoloader
-$loader = new Loader();
-$loader
-    ->setDirectories(
-        [
-            APP_PATH . '/controllers/',
-            APP_PATH . '/models/',
-        ]
-    )
-    ->register()
-;
+require_once $basePath . '/vendor/autoload.php';
 
-// Create a DI
-$container = new FactoryDefault();
-
-// Setting up the view component
-$container['view'] = function () {
-    $view = new View();
-    $view->setViewsDir(APP_PATH . '/views/');
-    return $view;
-};
-
-// Setup a base URI so that all generated URIs include the "tutorial" folder
-$container['url'] = function () {
-    $url = new UrlProvider();
-    $url->setBaseUri('/');
-    return $url;
-};
-
-// Set the database service
-$container['db'] = function () {
-    return new DbAdapter(
-        [
-            "host"     => 'tutorial-mysql',
-            "username" => 'phalcon',
-            "password" => 'secret',
-            "dbname"   => 'phalcon_tutorial',
-        ]
-    );
-};
+/** @var callable(): Application $factory */
+$factory = require $basePath . '/src/bootstrap.php';
 
 // Handle the request
 try {
-    $application = new Application($container);
-    $response    = $application->handle($_SERVER["REQUEST_URI"]);
-    $response->send();
-} catch (Exception $e) {
-    echo "Exception: ", $e->getMessage();
+    $application = $factory();
+    $application->handle($_SERVER['REQUEST_URI'])->send();
+} catch (Throwable $e) {
+    echo 'Exception: ', $e->getMessage();
 }
